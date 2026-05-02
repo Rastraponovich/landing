d9d45783a12f95f63ecdef332123278a5c4010b3
Rastraponovich <wilde@bk.ru>
2026-05-02 08:27:22 +0300

    feat: setup tailwindcss v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/webpage/', // Измени на имя твоего репозитория на GitHub
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
   build: {
     outDir: 'dist',
     sourcemap: true,
